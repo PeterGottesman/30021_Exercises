8994dad5b359c18e2424350a918fd44190dbd667
Peter Gottesman <peter@petergottesman.com>
2019-09-05 11:24:06 -0400

    Abstacted pin initialization

--- src/main.c
+++ src/main.c
@@ -15,27 +15,118 @@
 #include "string.h"
 #include "30021_io.h"
 
-void initJoystick(void)
+#define DEBUG 1
+
+#define PIN_MODE_INPUT 0x0
+#define PIN_MODE_OUTPUT 0x1
+#define PIN_MODE_ALTERNATE 0x2
+#define PIN_MODE_ANALOG 0x3
+#define PIN_MODE_RESET PIN_MODE_INPUT
+
+#define PIN_OTYPE_PP 0x0
+#define PIN_OTYPE_OD 0x1
+#define PIN_OTYPE_RESET PIN_OTYPE_PP
+
+#define PIN_PUPD_NONE 0x0
+#define PIN_PUPD_PU 0x1
+#define PIN_PUPD_PD 0x2
+#define PIN_PUPD_RESERVED 0x3
+
+#ifndef DEBUG
+#define DEBUG 0
+#endif
+
+#define pr_dbg(fmt, ...) if (DEBUG) printf(fmt, __VA_ARGS__)
+
+int initPin(GPIO_TypeDef *GPIO, uint8_t pin, uint8_t mode, uint8_t pupd, uint8_t otype)
 {
-    RCC->AHBENR |= RCC_AHBPeriph_GPIOA | RCC_AHBPeriph_GPIOB | RCC_AHBPeriph_GPIOC; // Enable clock for GPIO Port A
+    int shift;
+    if (pin > 15) {
+	return -1;
+    }
 
-    // Set pin PC0 (right) to input
-    GPIOC->MODER &= ~(0x0000000F); // Clear mode register
-    GPIOC->MODER |= (0x000000000); // Set mode register (0x00 - Input, 0x01 - Output, 0x02 - Alternate Function, 0x03 - Analog in/out)
-    GPIOC->PUPDR &= ~(0x0000000F); // Clear push/pull register
-    GPIOC->PUPDR |= (0x0000000A); // Set push/pull register (0x00 - No pull, 0x01 - Pull-up, 0x02 - Pull-down)
+    // Turn on clock for the port
+    switch ((unsigned long)GPIO)
+    {
+	case (unsigned long)GPIOA:
+	    RCC->AHBENR |= RCC_AHBPeriph_GPIOA;
+	    break;
+	case (unsigned long)GPIOB:
+	    RCC->AHBENR |= RCC_AHBPeriph_GPIOB;
+	    break;
+	case (unsigned long)GPIOC:
+	    RCC->AHBENR |= RCC_AHBPeriph_GPIOC;
+	    break;
+	case (unsigned long)GPIOD:
+	    RCC->AHBENR |= RCC_AHBPeriph_GPIOD;
+	    break;
+	case (unsigned long)GPIOE:
+	    RCC->AHBENR |= RCC_AHBPeriph_GPIOE;
+	    break;
+	case (unsigned long)GPIOF:
+	    RCC->AHBENR |= RCC_AHBPeriph_GPIOF;
+	    break;
+	default:
+	    pr_dbg("GPIO 0x%p not recognized\n", GPIO);
+	    return -1;
+    }
 
-    // Set pin PA4 (up) to input
-    GPIOA->MODER &= ~(0x00000300); // Clear mode register
-    GPIOA->MODER |= (0x00000000); // Set mode register (0x00 - Input, 0x01 - Output, 0x02 - Alternate Function, 0x03 - Analog in/out)
-    GPIOA->PUPDR &= ~(0x00000300); // Clear push/pull register
-    GPIOA->PUPDR |= (0x00000200); // Set push/pull register (0x00 - No pull, 0x01 - Pull-up, 0x02 - Pull-down)
+    shift = 2 * pin;
+    MODIFY_REG(GPIO->MODER, 0x3 << shift, mode << shift);
+    MODIFY_REG(GPIO->PUPDR, 0x3 << shift, pupd << shift);
+    if (mode == PIN_MODE_OUTPUT || mode == PIN_MODE_ALTERNATE)
+    {
+	MODIFY_REG(GPIO->OTYPER, 0x3 << shift, otype << shift);
+    }
 
-    // Set pin PB5 (center) to input
-    GPIOB->MODER &= ~(0x000000C03); // Clear mode register
-    GPIOB->MODER |= (0x00000000); // Set mode register (0x00 - Input, 0x01 - Output, 0x02 - Alternate Function, 0x03 - Analog in/out)
-    GPIOB->PUPDR &= ~(0x00000C03); // Clear push/pull register
-    GPIOB->PUPDR |= (0x00000802); // Set push/pull register (0x00 - No pull, 0x01 - Pull-up, 0x02 - Pull-down)
+    return 0;
+}
+
+int initJoystick(void)
+{
+    int ret;
+	    
+    // Right
+    ret = initPin(GPIOC, 0, PIN_MODE_INPUT, PIN_PUPD_PD, PIN_OTYPE_RESET);
+    if (ret)
+    {
+	pr_dbg("Failed to initialize PC0\n");
+	return ret;
+    }
+
+    // Up
+    ret = initPin(GPIOA, 4, PIN_MODE_INPUT, PIN_PUPD_PD, PIN_OTYPE_RESET);
+    if (ret)
+    {
+	pr_dbg("Failed to initialize PA4\n");
+	return ret;
+    }
+
+    // Center
+    ret = initPin(GPIOB, 5, PIN_MODE_INPUT, PIN_PUPD_PD, PIN_OTYPE_RESET);
+    if (ret)
+    {
+	pr_dbg("Failed to initialize PB5\n");
+	return ret;
+    }
+    
+    // Left
+    ret = initPin(GPIOC, 1, PIN_MODE_INPUT, PIN_PUPD_PD, PIN_OTYPE_RESET);
+    if (ret)
+    {
+	pr_dbg("Failed to initialize PC1\n");
+	return ret;
+    }
+    
+    // Down
+    ret = initPin(GPIOB, 0, PIN_MODE_INPUT, PIN_PUPD_PD, PIN_OTYPE_RESET);
+    if (ret)
+    {
+	pr_dbg("Failed to initialize PB0\n");
+	return ret;
+    }
+
+    return 0;
 }
 
 uint8_t readJoystick(void)
@@ -55,11 +146,18 @@ uint8_t readJoystick(void)
 
 int main(void)
 {
-    init_usb_uart(9600);
-    puts("UART Initialized\n");
+    int ret;
 
-    initJoystick();
-    puts("Joystick Initialized\n");
+    init_usb_uart(9600);
+    pr_dbg("UART Initialized\n");
+
+    ret = initJoystick();
+    if (ret)
+    {
+	pr_dbg("Failed to initialize joystick\n");
+    } else {
+	pr_dbg("Joystick Initialized\n");
+    }
 
     uint8_t last = -1;
 
