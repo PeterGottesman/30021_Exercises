9f4ae2c4281e6d822b51d1fe26608dee8a5acc68
Peter Gottesman <peter@petergottesman.com>
2019-10-01 09:40:26 -0400

    christine reads slow

--- Ex_3/src/main.c
+++ Ex_3/src/main.c
@@ -11,13 +11,19 @@
    ID:                 $Id:  $
 
 **********************************************************************/
+
 #include "stm32f30x_conf.h"
 #include "30021_io.h"
 #include "gpio.h"
+#include "lcd.h"
+#include "adc.h"
+#include <string.h>
 #include <stdio.h>
 
 #define Ftimer 64000000
 #define Fck 2560000
+#define FULL_SCALE 4095
+#define VREFINT_CAL *((uint16_t*) ((uint32_t) 0x1FFFF7BA))   //calibrated at 3.3V@ 30C
 
 int initTimer(void)
 {
@@ -43,29 +49,56 @@ int initTimer(void)
 	       TIM_CCMR1_OC1PE | TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2);
 
     MODIFY_REG(TIM16->BDTR, TIM_BDTR_MOE | TIM_BDTR_OSSI | TIM_BDTR_OSSR, TIM_BDTR_MOE);
-	       
+
 
     // Set output enable and polarity
     MODIFY_REG(TIM16->CCER, TIM_CCER_CC1P  | TIM_CCER_CC1E, TIM_CCER_CC1E);
-    
+
     // Enable
     TIM16->CR1 |= 0x1;
 
     // Disable interrupt
     TIM16->DIER &= ~0x0001;
-	
+
     return 0;
 }
 
 int main(void)
 {
+    float Vdda;
+    uint16_t Vref_data;
+
+    char meas1[26];
+    uint8_t fbuffer[512];
+
     initTimer();
     initPinAlternate(GPIOA, GPIO_PinSource6, GPIO_AF_1);
-    /* RCC->AHBENR |= RCC_AHBPeriph_GPIOB; */
-    /* MODIFY_REG(GPIOB->MODER, GPIO_MODER_MODER6, GPIO_MODER_MODER6_1); */
-    /* MODIFY_REG(GPIOB->PUPDR, GPIO_PUPDR_PUPDR6, 0); */
-    /* MODIFY_REG(GPIOB->OSPEEDR, GPIO_OSPEEDER_OSPEEDR6, */
-    /* 	       GPIO_OSPEEDER_OSPEEDR6_1 | GPIO_OSPEEDER_OSPEEDR6_0); */
-    /* GPIO_PinAFConfig(GPIOA, GPIO_PinSource11, GPIO_AF_1); */
-    while (1){}
+
+    initPin(GPIOA,0,PIN_MODE_INPUT,PIN_PUPD_NONE, PIN_OTYPE_RESET);
+    init_spi_lcd();
+    memset(fbuffer,0x00,512);
+    lcd_push_buffer(fbuffer);
+
+    initADC(1);
+
+    ADC1_2->CCR |= ADC12_CCR_VREFEN;
+    ADC_RegularChannelConfig(ADC1, ADC_Channel_18, 1, ADC_SampleTime_181Cycles5);
+    ADC_StartConversion(ADC1); // Start ADC read
+    while (ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC) == 0); // Wait for ADC read
+    Vref_data = ADC_GetConversionValue(ADC1); // Read the ADC value
+    Vdda = 3.3 * VREFINT_CAL/Vref_data;
+
+    while(1)
+    {
+//	if(display==1)
+	{
+	    uint16_t CH1 = ADC_measure_PA(ADC_Channel_1);
+	    float Vc1 = Vdda/FULL_SCALE * CH1;
+	    snprintf(meas1,25,"%.2fV", Vc1);
+	    lcd_write_string("OUTPUT1",fbuffer,0,0);
+	    lcd_write_string(meas1,fbuffer,3,1);
+	    lcd_push_buffer(fbuffer);
+//	    display=0;
+	}
+    }
 }
