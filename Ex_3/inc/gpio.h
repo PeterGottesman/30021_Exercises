072f8dc89bc17f70417817a4914fd71bbe1d5b2b
Peter Gottesman <peter@petergottesman.com>
2019-10-01 08:57:56 -0400

    Ex3.1

---
+++ Ex_3/inc/gpio.h
@@ -0,0 +1,27 @@
+#ifndef _GPIO_H
+#define _GPIO_H
+#include "stm32f30x_conf.h"
+
+#define PIN_MODE_INPUT 0x0
+#define PIN_MODE_OUTPUT 0x1
+#define PIN_MODE_ALTERNATE 0x2
+#define PIN_MODE_ANALOG 0x3
+#define PIN_MODE_RESET PIN_MODE_INPUT
+
+#define PIN_OTYPE_PP 0x0
+#define PIN_OTYPE_OD 0x1
+#define PIN_OTYPE_RESET PIN_OTYPE_PP
+
+#define PIN_PUPD_NONE 0x0
+#define PIN_PUPD_PU 0x1
+#define PIN_PUPD_PD 0x2
+#define PIN_PUPD_RESERVED 0x3
+
+int initPin(GPIO_TypeDef *GPIO, uint8_t pin, uint8_t mode, uint8_t pupd, uint8_t otype);
+int initLed(void);
+void setLed(uint8_t color);
+int initJoystick(void);
+uint8_t readJoystick(void);
+
+#endif
+
