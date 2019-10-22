61f51b898df7fb6ff4364bc8be892067ecd824f8
Peter Gottesman <peter@petergottesman.com>
2019-10-22 09:49:25 -0400

    Some fucked up code, enjoy

--- Ex_5/src/main.c
+++ Ex_5/src/main.c
@@ -13,13 +13,20 @@
 **********************************************************************/
 #include "stm32f30x_conf.h"
 #include "30021_io.h"
+#include "gyro.h"
 #include <stdio.h>
 
 int main(void)
 {
+    uint16_t x,y,z;
     init_usb_uart(9600);
-    printf("Hello, world!\n");
-  while(1)
-  {
-  }
+    init_spi_gyro();
+
+    x = y = z = 0;
+    while(1)
+    {
+	gyro_read(&x, &y, &z);
+	printf("x: %02x, y: %02x, z: %02x\n", x, y, z);
+	for(int i = 0; i < 1000000; ++i);
+    }
 }
